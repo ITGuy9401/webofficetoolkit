5a73bc3822e36aec94c804d941abdd91add2e088
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-03-28 13:57:45 +0200

    dev

--- database-config.js
+++ database-config.js
@@ -16,149 +16,153 @@
 		.authenticate()
 		.then(() => {
 			console.info('Connection to the database has been established successfully.');
+			dbConfig();
 		}, (err) => {
 			console.error('Unable to connect to the database:', err);
 		});
 
-	var db = {};
 
-	// Company Entity
-	db.Company = sequelize.define('company', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING
-		},
-		vat: {
-			type: Sequelize.STRING
-		}
-	});
+	function dbConfig() {
+		var db = {};
 
-	// Office Entity
-	db.Office = sequelize.define('office', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING
-		},
-		location: {
-			type: Sequelize.STRING
-		}
-	});
+		// Company Entity
+		db.Company = sequelize.define('company', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			vat: {
+				type: Sequelize.STRING
+			}
+		});
 
-	db.Office.belongsTo(db.Company, {
-		foreignKey: 'companyId'
-	});
+		// Office Entity
+		db.Office = sequelize.define('office', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			location: {
+				type: Sequelize.STRING
+			}
+		});
 
-	db.Company.hasOne(db.Office, {
-		foreignKey: 'mainOffice',
-		targetKey: 'companyId'
-	});
+		db.Office.belongsTo(db.Company, {
+			foreignKey: 'companyId'
+		});
+
+		db.Company.hasOne(db.Office, {
+			foreignKey: 'mainOffice',
+			targetKey: 'companyId'
+		});
 
 
-	// Employee Entity
-	db.Employee = sequelize.define('employee', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING
-		},
-		surname: {
-			type: Sequelize.STRING
-		},
-		sex: {
-			type: Sequelize.STRING
-		},
-		birthdate: {
-			type: Sequelize.DATE
-		},
-		photo: {
-			type: Sequelize.BLOB
-		}
-	});
+		// Employee Entity
+		db.Employee = sequelize.define('employee', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			surname: {
+				type: Sequelize.STRING
+			},
+			sex: {
+				type: Sequelize.STRING
+			},
+			birthdate: {
+				type: Sequelize.DATE
+			},
+			photo: {
+				type: Sequelize.BLOB
+			}
+		});
 
-	db.Employee.belongsTo(db.Company, {
-		foreignKey: 'companyId'
-	});
+		db.Employee.belongsTo(db.Company, {
+			foreignKey: 'companyId'
+		});
 
-	// Presence Entity
+		// Presence Entity
 
-	db.Presence = sequelize.define('presence', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		entranceDate: {
-			type: Sequelize.DATE
-		},
-		exitDate: {
-			type: Sequelize.DATE
-		}
-	});
+		db.Presence = sequelize.define('presence', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			entranceDate: {
+				type: Sequelize.DATE
+			},
+			exitDate: {
+				type: Sequelize.DATE
+			}
+		});
 
-	db.Employee.belongsTo(db.Presence, {
-		foreignKey: 'employeeId'
-	});
+		db.Employee.belongsTo(db.Presence, {
+			foreignKey: 'employeeId'
+		});
 
-	db.Office.belongsTo(db.Presence, {
-		foreignKey: 'officeId'
-	});
+		db.Office.belongsTo(db.Presence, {
+			foreignKey: 'officeId'
+		});
 
-	// Role entity
-	db.Role = sequelize.define('role', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING
-		},
-		code: {
-			type: Sequelize.STRING,
-			unique: true
-		}
-	});
+		// Role entity
+		db.Role = sequelize.define('role', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			code: {
+				type: Sequelize.STRING,
+				unique: true
+			}
+		});
 
-	db.Office.belongsTo(db.Role, {
-		foreignKey: 'companyId'
-	});
+		db.Office.belongsTo(db.Role, {
+			foreignKey: 'companyId'
+		});
 
-	// EmployeeRole entity
-	db.EmplyeeRole = sequelize.define('employeeRole', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		startDate: {
-			type: Sequelize.DATE
-		},
-		endDate: {
-			type: Sequelize.DATE
-		}
-	});
+		// EmployeeRole entity
+		db.EmplyeeRole = sequelize.define('employeeRole', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			startDate: {
+				type: Sequelize.DATE
+			},
+			endDate: {
+				type: Sequelize.DATE
+			}
+		});
 
-	db.Role.belongsTo(db.EmployeeRole, {
-		foreignKey: 'roleId'
-	});
+		db.Role.belongsTo(db.EmployeeRole, {
+			foreignKey: 'roleId'
+		});
 
-	db.Employee.belongsTo(db.EmployeeRole, {
-		foreignKey: 'employeeId'
-	});
+		db.Employee.belongsTo(db.EmployeeRole, {
+			foreignKey: 'employeeId'
+		});
 
-	
-	// DOMAIN END
-	// Exporting all database entities to the main application
-	exports.database = db;
+
+		// DOMAIN END
+		// Exporting all database entities to the main application
+		exports.database = db;
+	}
 })(require, exports);
