103cf555c9f903f3ac80853ba33cd5e68a329109
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-03-28 13:37:16 +0200

    dev

--- database-config.js
+++ database-config.js
@@ -2,83 +2,118 @@
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('main', null, null, {
-    // sqlite! now!
-    dialect: 'sqlite',
+	// sqlite! now!
+	dialect: 'sqlite',
 
-    // the storage engine for sqlite
-    // - default ':memory:'
-    storage: 'webofficetoolkit.sqlite'
+	// the storage engine for sqlite
+	// - default ':memory:'
+	storage: 'webofficetoolkit.sqlite'
 });
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.info('Connection to the database has been established successfully.');
-    }, (err) => {
-        console.error('Unable to connect to the database:', err);
-    });
+	.authenticate()
+	.then(() => {
+		console.info('Connection to the database has been established successfully.');
+	}, (err) => {
+		console.error('Unable to connect to the database:', err);
+	});
 
 var db = {};
 
 // Company Entity
 
 db.Company = sequelize.define('company', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    vat: {
-        type: Sequelize.STRING
-    },
-    mainOffice: {
-        type: Sequelize.INTEGER
-    }
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: {
+		type: DataTypes.STRING
+	},
+	vat: {
+		type: DataTypes.STRING
+	}
 });
 
 // Office Entity
 
 db.Office = sequelize.define('office', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    location: {
-        type: Sequelize.STRING
-    },
-    companyId: {
-        type: Sequelize.INTEGER
-    }
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: {
+		type: DataTypes.STRING
+	},
+	location: {
+		type: DataTypes.STRING
+	}
 });
 
-db.Office.belongsTo(db.Company, {foreignKey: 'companyId'}); 
+db.Office.belongsTo(db.Company, {
+	foreignKey: 'companyId'
+});
 
-db.Company.hasOne(db.Office, {foreignKey: 'mainOffice', targetKey: 'companyId'}); 
+db.Company.hasOne(db.Office, {
+	foreignKey: 'mainOffice',
+	targetKey: 'companyId'
+});
 
 
 // Employee Entity
 
 db.Employee = sequelize.define('employee', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    companyId: {
-        type: Sequelize.INTEGER
-    }
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: {
+		type: DataTypes.STRING
+	},
+	surname: {
+		type: DataTypes.STRING
+	},
+	sex: {
+		type: DataTypes.STRING
+	},
+	birthdate: {
+		type: DataTypes.DATE
+	},
+	photo: {
+		type: DataTypes.BLOB
+	}
 });
 
-db.Employee.belongsTo(db.Company, {foreignKey: 'companyId'});
+db.Employee.belongsTo(db.Company, {
+	foreignKey: 'companyId'
+});
 
-db.Presence = sequelize.define('presence')
+// Presence Entity
+
+db.Presence = sequelize.define('presence', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	entranceDate: {
+		type: DataTypes.DATE
+	},
+	exitDate: {
+		type: DataTypes.DATE
+	}
+});
+
+db.Employee.belongsTo(db.Presence, {
+	foreignKey: 'employeeId'
+});
+
+db.Office.belongsTo(db.Presence, {
+	foreignKey: 'officeId'
+});
 
 // DOMAIN END
 exports.database = db;
